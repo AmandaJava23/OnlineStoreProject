85b78c0c6ee3a6f90161b084363fe2cd68028236
EliasSan <saile.dragednas@proton.me>
2024-02-13 15:30:45 +0100

    adding Product class

--- src/main/java/se/yrgo/Product.java
+++ src/main/java/se/yrgo/Product.java
@@ -1,5 +1,25 @@
 package se.yrgo;
 
 public class Product {
+    private String name;
+    private String artNo;
+    private double price;
 
+    public Product(String name, String artNo, double price) {
+        this.name = name;
+        this.artNo = artNo;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArtNo() {
+        return artNo;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
