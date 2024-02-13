bb111f92617b859cde84c14dd90f0615a8e6f203
EliasSan <saile.dragednas@proton.me>
2024-02-13 15:57:01 +0100

    creating ProductTest

--- src/test/java/se/yrgo/ProductTest.java
+++ src/test/java/se/yrgo/ProductTest.java
@@ -1,6 +1,26 @@
 package se.yrgo;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductTest {
 
+    @Test
+    public void productNameTest() {
+        Product axe = new Product("axe", "SA234325", 140.00);
+        assertEquals("axe", axe.getName());
+    }
+
+    Product product2 = new Product("product2", "SA234325", 140.00);
+
+    @Test
+    public void productArtNoTest() {
+        Product axe = new Product("axe", "SA234325", 140.00);
+        assertEquals("SA234325", axe.getArtNo());
+    }
+    @Test
+    public void productPriceTest() {
+        Product axe = new Product("axe", "SA234325", 140.00);
+        assertEquals(140.00, axe.getPrice());
+    }
 }
