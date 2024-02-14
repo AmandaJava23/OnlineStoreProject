0e9312dac729080a827101f1aa730763eb5df24d
Amanda Vasilis <amavas0414@skola.goteborg.se>
2024-02-14 16:04:28 +0100

    added javadoc in ShoppingCart

--- src/main/java/se/yrgo/ShoppingCart.java
+++ src/main/java/se/yrgo/ShoppingCart.java
@@ -10,14 +10,27 @@ public class ShoppingCart {
         this.shoppingCart = new ArrayList<>();
     }
 
+    /**
+     * Adding a product to the shopping cart.
+     * @param product The product that will be added to the shopping cart.
+     */
     public void addProductToCart(Product product) {
         shoppingCart.add(product);
     }
 
+    /**
+     * Removes a product from the shopping cart.
+     * @param product The product that will be removed from the shopping cart.
+     */
     public void removeProductFromCart(Product product) {
         shoppingCart.remove(product);
     }
 
+    /**
+     * Calculates the total value of the items in the shopping cart.
+     * @return The total value of the items in the shopping cart.
+     * @throws NullPointerException If the shopping cart is empty (null).
+     */
     public double valueOfCart() throws NullPointerException{
         if (shoppingCart == null ) {
             throw new NullPointerException("Shoppingcart is empty!");
@@ -30,6 +43,11 @@ public class ShoppingCart {
         return valueOfCart;
     }
 
+
+    /**
+     * Prints the products name and price in the shopping cart.
+     *
+     */
     public void showShoppingCart() {
         for (Product product : shoppingCart) {
             System.out.println(product.getName() + ", Price: " + product.getPrice());
